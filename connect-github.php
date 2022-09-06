fb6ff0aa29a2a2d556f64eed718d4454ce025d91
tithi_rp <tithi92.rp@gmail.com>
2022-09-06 13:15:39 +0530

    test

---
+++ connect-github.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = 'https://github.com/login/oauth/authorize';
+$params = [
+   'client_id' => 'a369fba81f12d2a6577f',
+   'redirect_url' => 'https://test.agile24.net/github_oauth.php',
+   'scope' => 'repo read:org read:user user:email read:project',
+   'state' => '134567'
+];
+$url .= "?" . http_build_query($params);
+
+echo '<a href="'.$url.'">Connect with GitHub</a>';
